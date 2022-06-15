a695c7b0d6c34ac69123e25438a4e8ef300a3f76
OGShawnLee <changoloco.evg@gmail.com>
2022-06-15 01:11:50 +0000

    :lipstick: feat(ui): truncate country name

--- src/components/Country.svelte
+++ src/components/Country.svelte
@@ -13,7 +13,9 @@
 		height="160"
 	/>
 	<div class="pt-8 px-8 pb-14">
-		<h2 class="mb-4 | text-lg font-bold capitalize">{country.name}</h2>
+		<h2 class="mb-4 | text-lg font-bold capitalize">
+			{shortenString(country.name, 35)}
+		</h2>
 		<dl class="space-y-2">
 			<div class="flex gap-1">
 				<dt class="font-semibold">Population:</dt>
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,3 +2,8 @@
 export function numberWithCommas(x: number | string) {
 	return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
 }
+
+export function shortenString(str: string, length: number) {
+	if (str.length <= length) return str;
+	return str.substring(0, length) + '...';
+}
